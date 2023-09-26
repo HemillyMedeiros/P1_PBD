354c756d222ecbeca0c2c91371837643a0e5089a
Hemilly Medeiros <hemilly.santos@fatec.sp.gov.br>
2023-09-26 10:07:45 -0300

    feat(cur_und):cálculo de preço médio por país

--- P1.sql
+++ P1.sql
@@ -6,3 +6,36 @@ CREATE TABLE wine_reviews (
     points INTEGER,
     price NUMERIC
 );
+
+
+-- 2.Calcular o preço médio dos vinhos de cada país utilizando um cursor não vinculado.
+
+--Criação da tabela para armazenar os resultados
+CREATE TABLE wine_avg_price_by_country (
+    id SERIAL PRIMARY KEY,
+    nome_pais VARCHAR(255),
+    preco_medio NUMERIC
+);
+
+-- Criação da função para cálculo do preço médio por país
+CREATE OR REPLACE FUNCTION calculate_avg_price_by_country()
+RETURNS VOID AS $$
+DECLARE
+    country_name VARCHAR(255);
+    avg_price NUMERIC;
+BEGIN
+    FOR country_name IN (SELECT DISTINCT country FROM wine_reviews)
+    LOOP
+        SELECT AVG(price) INTO avg_price
+        FROM wine_reviews
+        WHERE country = country_name;
+
+        INSERT INTO wine_avg_price_by_country (nome_pais, preco_medio)
+        VALUES (country_name, avg_price);
+    END LOOP;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Chamar a função para calcular o preço médio por país
+SELECT calculate_avg_price_by_country();
+]
