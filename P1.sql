0972309f92ccd73508fca581f689dd7e8c008fe2
Hemilly Medeiros <hemilly.santos@fatec.sp.gov.br>
2023-09-26 10:10:35 -0300

    feat(cur_bound):obtenção de descrição mais longa por país

--- P1.sql
+++ P1.sql
@@ -39,3 +39,36 @@ $$ LANGUAGE plpgsql;
 -- Chamar a função para calcular o preço médio por país
 SELECT calculate_avg_price_by_country();
 ]
+
+--3.Identificar a descrição mais longa para os vinhos de cada país utilizando um cursor vinculado.
+
+-- Criação da tabela para armazenar os resultados
+CREATE TABLE longest_description_by_country (
+    id SERIAL PRIMARY KEY,
+    nome_pais VARCHAR(255),
+    descricao_mais_longa TEXT
+);
+
+-- Criação da função para identificar a descrição mais longa por país
+CREATE OR REPLACE FUNCTION find_longest_description_by_country()
+RETURNS VOID AS $$
+DECLARE
+    country_name VARCHAR(255);
+    max_description TEXT;
+BEGIN
+    FOR country_name IN (SELECT DISTINCT country FROM wine_reviews)
+    LOOP
+        SELECT description INTO max_description
+        FROM wine_reviews
+        WHERE country = country_name
+        ORDER BY LENGTH(description) DESC
+        LIMIT 1;
+
+        INSERT INTO longest_description_by_country (nome_pais, descricao_mais_longa)
+        VALUES (country_name, max_description);
+    END LOOP;
+END;
+$$ LANGUAGE plpgsql;
+
+ -- Chamar a função para encontrar a descrição mais longa por país
+SELECT find_longest_description_by_country();
